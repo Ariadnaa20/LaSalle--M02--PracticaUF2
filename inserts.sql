56d6ac5d2c7d45a0186038c2033be204b3b68049
karen <karenquispego97@gmail.com>
2024-02-08 09:53:35 +0100

    Hecha tabla persona, ahora si

--- inserts.sql
+++ inserts.sql
@@ -1,6 +1,28 @@
 --Karen Quispe i Ariadna Pascual
 
-se supone que esto se guarda en la rama Select
+INSERT INTO PERSONA VALUES
+("123456789Q", "Yunus", "Farag", " Archimedes", "Turca", "H", "12-04-1987")
+("234567891W", "Coenraad", "Elhanan", "Tavish",  "Brasileña", "H", "03-04-1990")
+("345678912E", "Raúl", "Szebastiá", "Veit", "Ruso", "H", "01-11-1999")
+("456789112R", "Anargyros", "Ougein", "Ásbjörn", "Danes", "H", "09-05-2001")
+("567891234T", "Lilian", "Hama", "Bruno", "H", "Sudafricana", "23-08-1998")
 
+("678912345Y", "Nicodemus", "Achilleus", "Illiam", "Turco", "H", "19-05-1998")
+("789123456U", "Brantley", " Tim ", "Teboho", "Marroqui", "H", "29-12-1994")
+("891234567I", "Timo", "Kōji", "Senán", "Canadiense", "H", "04-08-1992")
+("912345678P", "Lula", " Kalani", "Nyyrikki", "Japonesa", "H", "24-09-1999")
+("987654321A", "Dudu", "Cynebald ", "Mathis", "Ruso", "H", "21-08-1993")
 
+("876543219S", "Rafiq", " Malone", "Liron", "Español", "H", "16-02-2000")
+("765432198D", "Saeed", "Xanthos", "Izan", "Danes", "H", "12-09-1998")
+("654321987F", "Abiram", "Balor", "Rutendo", "Frances", "H", "13-07-2001")
+("543219876G", "Svante", "Jeremiel", "Rini", "Italiana", "H", "22-04-2003")
+("432198765H", "Juurou", "Rafiq", "Jani", "Sudafricana", "H", "04-04-1996")
+
+("", "Emiel", "Gibson", "Danr", "Mexicana", "H", "17-12-2002")
+("", "Hubertus", "Umaru", "Ingvar", "Canadiense", "H", "15-03-1997")
+("", "Janek", "Marc", "Henrikas", "Brasileña", "H", "19-02-2000")
+("", "Liam", "Genadi", "Quidel", "Australiana", "H", "08-02-2000")
+("", "Anaru", "Búi", "Cheops", "Japonesa", "H", "29-08-2000")
+("", "", "", "", "", "", "")
 
