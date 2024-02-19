c0e751ab625643c7285835a9d8602552f72b384e
karen <karenquispgo97@gmail.com>
2024-02-19 10:20:38 +0100

    actualizacion de selects

--- SELECTS.sql
+++ SELECTS.sql
@@ -79,28 +79,76 @@ HAVING COUNT(*) >= 3;
 --12 Retorna amb el país i any els equips nacionals amb el nom i cognom del seu entrenador. Fes-ho pels anys del 2010 al 2015 i pels països que comencin per A. Quants entrenadors retorna la consulta?
 SELECT nationalteam.Year, Country, CONCAT(person.Name, ' ', person.Surname) AS CoachName
 FROM nationalteam
+JOIN franchise_season ON nationalteam.Year = franchise_season.RegularSeasonYear
+JOIN franchise ON franchise_season.FranchiseName = franchise.Name
+JOIN headcoach ON franchise.IDCardCoach = headcoach.IDCard
 JOIN person ON headcoach.IDCard = person.IDCard
-JOIN headcoach ON nationalteam.IDCardHeadCoach = headcoach.IDCard
 WHERE nationalteam.Year BETWEEN 2010 AND 2015 AND Country LIKE 'A%';
 
+Retorna 25 files y un total de 5 entrenadors
 
 
 --13 Per un any específic retorna per cada equip la suma dels salaris dels seus jugadors. Asumeix que tots els jugadors que tenen un contracte en qualsevol data de l'any 2007 s'ha de contabilitzar. Quin és el presupost dels Houston Rockets?
+SELECT f.Name AS TeamName, SUM(pf.Salary) AS TotalSalary
+FROM player_franchise pf
+JOIN player p ON pf.IDCardPlayer = p.IDCard
+JOIN franchise f ON pf.FranchiseName = f.Name
+WHERE YEAR(pf.StartContract) = 2007 OR YEAR(pf.EndContract) = 2007
+GROUP BY f.Name;
 
+
+El presupost de Hubertus Rockets es: 25654886
 
 --14 Retorna cada arena amb la seva capacitat, juntament amb el nombre de seients que tenen. Quants seients té el Footprint Center?
 
---15 Crea un informe amb tots els jugadors que no son dels Estats Units ni d'Espanya. Inclou-ne tota la seva informació personal completa. Ordena els resultats per nacionalitat i data de naixement ascendent. Quina és la ID del terncer juador retornat?
+-- esta en la base de datos
 
+--15 Crea un informe amb tots els jugadors que no son dels Estats Units ni d'Espanya. Inclou-ne tota la seva informació personal completa. Ordena els resultats per nacionalitat i data de naixement ascendent. Quina és la ID del terncer juador retornat?
+SELECT p.IDCard, per.Name, p.PRODebutYear, p.UniversityOfOrigin, p.NBARings, per.BirthDate, per.Nationality
+FROM person per
+JOIN player p ON per.IDCard = p.IDCard
+WHERE per.Nationality NOT IN ('United States', 'Spain')
+ORDER BY per.Nationality, per.BirthDate ASC;
+
+La ID del tercer jugador es: 100050477
 --16 Mostra un informe amb el nom, cognom i data de naixement de tots els caps d'entrenadors assistents de l'especialitat de psicologia sense repetits i que tenen una data de naixement registrada. Ordena per cognom i nom. Quin és l'any de naixement del tercer resultat?
+SELECT DISTINCT per.Name AS AssistantCoachName, per.Surname AS AssistantCoachSurname, per.BirthDate AS BirthDate
+FROM person per
+JOIN assistantcoach ac ON per.IDCard = ac.IDCard
+WHERE ac.Especiality = 'Psicologia' AND per.BirthDate IS NOT NULL
+ORDER BY per.Surname, per.Name;
+
+--no me sale ningun resultado al realizar la consulta
 
 --17 Volem saber quantes franquícies hi ha per a cada conferència. Mostra totes les dades relacionades amb la conferència i un nou camp amb el recompte. Quantes franquícies hi ha acada conferència?
+SELECT c.Name AS ConferenceName, c.GeographicZone, a.Name AS ArenaName, a.City AS ArenaCity, COUNT(f.Name) AS FranchiseCount
+FROM conference AS c
+JOIN franchise AS f ON c.Name = f.ConferenceName
+JOIN arena AS a ON f.ArenaName = a.Name
+GROUP BY c.Name, c.GeographicZone
+ORDER BY c.Name;
 
+A cada conferencia hi ha 15 franquicia
 --18 Sabent que molts jugadors han estat seleccionats en algun moment per les seves seleccions, retorna tots els jugadors que han estat seleccionats en l'any 2010. Inclou IDCard, Nom, Cognom, Nacionalitat, Any de selecció, en aquell mateix any i el número de samarreta en la selecció. Ordena el resultat pel numero de samarreta. Quina es al nacionalitat del primer resultat que apareix?
+SELECT p.IDCard, pr.Name, pr.Surname, nt.Country, ntp.Year, ntp.ShirtNumber
+FROM player p
+JOIN person pr ON p.IDCard = pr.IDCard
+JOIN nationalteam_player ntp ON p.IDCard = ntp.IDCard
+JOIN nationalteam nt ON ntp.Country = nt.Country
+WHERE ntp.Year = 2010 ORDER BY ntp.ShirtNumber;
+
+La nacionalitat es: canada
 
 --19 Retorna el ID, nom i cognom dels jugadors i les dades del seu draft si es que en tenen. Ordena per cognom i any de draft. Quina es la ID del primer resultat?
+SELECT p.IDCard AS PlayerID, pr.Name AS Name, pr.Surname AS Surname, d.DraftYear, d.IDCardPlayer AS DraftID
+FROM player p 
+JOIN person pr ON p.IDCard = pr.IDCard
+LEFT JOIN draft_player_franchise d ON p.IDCard = d.IDCardPlayer
+ORDER BY  Surname, DraftYear;
 
---20 Retorna les franquícies que han jugat a totes les temporades regulars registrades. Ordena alfabèticament de la Z a la A. I tornaúnicament el 3 resultat.Quin és el nom del equip?
+ID primer resultat: 100050166
+
+--20 Retorna les franquícies que han jugat a totes les temporades regulars registrades. Ordena alfabèticament de la Z a la A. I torna únicament el 3 resultat. Quin és el nom del equip?
 
 --21 Per cada especialitat d'entrenadors assistents, retorna quants n'ha tingut cada franquícia. Qunatsmetges tenen els Brooklin Nets?
 
@@ -111,3 +159,19 @@ WHERE nationalteam.Year BETWEEN 2010 AND 2015 AND Country LIKE 'A%';
 --24 Omple la columna NBARings de la taula de Franquícies. Aquest camp es pot calcular mitjançant la taula Franchise_Season contant quantes vegades han guanyat. Utilitza una declaració UPDATE. Un cop ho tingueu, trobeu quantes franquícies tenen 4 o més anells.
 
 --25 Troba el nom de la franquícia amb el valor de budjet més petit.
+
+--25 Troba la ciutat de l'arena que tingui més seients sempre i quan siguin més de 18000 (veure taules seat i arena, NO utilitzar Capacity)
+
+--27 Retorna la ID del jugador i el nom de la seva franquicia que han quedat primers al draft i al mateix any d'aquest han gunayat la temporada regular. Retorna la ID de l'únic que te Universitat d'origen.
+ 
+--28 Retorna els paisos amb més de 50 jugadors, 3 entrenadors i 10 assistents de paisos que tinguin selecció. Quin país apareix als resultats?
+
+--29 Retorna els headcoach que entrenin equips nacionals amb el salari més gran o el percentantge de victòria més petit, d'entre els que entrenen equips nacionals. Quan sumen els salaris dels entrenadors resultants?
+
+--30 Retorna els jugadors que han jugat en 2 equips o més i han estat convocats també a la selecció més d'un cop. Qunats jugadors hi ha en aquesta situació?
+
+--1301 Quina és la mitja del salary dels jugadors per any? (Agafa l'any que han començat els seus contractes per fer les mitjes i arrodoneix el resultat a 2 decimals). Quin és el valor resultant dels decimals de l'any 2009?
+
+--1401 En quin estadi la capacitat excedeit en mes de 50 el nombre de seients?
+
+--1701 Volem saber quants estadis hi ha per conferència. Mostra totes les dades relacionades amb la conferència i un nou camp amb el recompte d'estadis. Quants estadis tenim a la conferència oest?
